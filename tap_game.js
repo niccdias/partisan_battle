645a02120c8382f7f4b344b7511295d96a453e64
Nic Dias <really.niccdias@gmail.com>
2023-09-06 14:28:07 +0200

    Checking when postMessage is failing

--- tap_game.js
+++ tap_game.js
@@ -211,9 +211,10 @@ const end_game = () => {
 /// Start button
 const start_button_pressed = () => {
     if(!game_began) {
-        start_game()
+        start_game();
     } else{
-        window.parent.postMessage(player_outcome, '*');
+        console.log(player_outcome);
+        window.parent.postMessage(player_outcome.toString(), '*');
     }
 
     return 0;
@@ -238,9 +239,7 @@ const set_top_player_score = (click_increment) => {
 const check_for_winner = (times_up = false) => {
     // If top player has won
     if (get_top_margin() >= screen_height || (times_up && (get_top_margin() >= get_bottom_margin()) )) {
-        console.log(bottom_player_clicks);
         player_outcome = "L" + bottom_player_clicks.toString();
-        console.log(player_outcome);
         out_party_total = out_party_total + 0.25;
         top_player_total.innerHTML = "$" + out_party_total.toFixed(2);
         end_game();
@@ -248,9 +247,7 @@ const check_for_winner = (times_up = false) => {
     
     // If bottom player has won
     if (((get_top_margin() == 0 && (top_player_clicks || bottom_player_counts))) || (times_up && (get_top_margin() < get_bottom_margin()) )) {
-        console.log(bottom_player_clicks);
         player_outcome = "W" + bottom_player_clicks.toString();
-        console.log(player_outcome);
         in_party_total = in_party_total + 0.25;
         bottom_player_total.innerHTML = "$" + in_party_total.toFixed(2);
         end_game();
